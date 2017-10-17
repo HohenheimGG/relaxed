28a120e5cb5e44d134591af681bfb5c3fcba2439
hohenheim <chenzaocheng375@pingan.com.cn>
2017-10-17 14:05:56 +0800

    finish 120

--- leet_code/120_Triangle.py
+++ leet_code/120_Triangle.py
@@ -27,7 +27,21 @@ class Triangle:
             total += min
         return total
 
+    def solution2(self, nums):
+        if not nums:
+            return None
+        length = len(nums)
+        result = [0] * (length + 1)
+        layer = length - 1
+        while layer >= 0:
+            for index in range(0, len(nums[layer])):
+                result[index] = min(result[index], result[index + 1]) + nums[layer][index]
+            layer -= 1
+        print result
+        print '\n'
+        return result[0]
+
 if __name__ == '__main__':
-    nums = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
-    result = Triangle().solution1(nums)
+    nums = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3], [5, 1, 9, 3, 0]]
+    result = Triangle().solution2(nums)
     print result
