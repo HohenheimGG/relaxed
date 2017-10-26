9376db2af0aac7c8411c8dc62138224c7f009e1a
Hohenheim <eph.oliver@gmail.com>
2017-10-26 21:44:53 +0800

    init 21

---
+++ leet_code/21_merge_two_sorted_lists.py
@@ -0,0 +1,9 @@
+# Merge two sorted linked lists and return it as a new list.
+# The new list should be made by splicing together the nodes of the first two lists.
+
+class MergeTwoSortedList:
+    def solution1(self):
+        pass
+
+if __name__ == "__main__":
+    pass
