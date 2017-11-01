911d170389ed4e168ed7bcd8ea83690f503d7c58
Hohenheim <eph.oliver@gmail.com>
2017-11-01 23:40:34 +0800

    finish  236

--- leet_code/236_lowest_common_ancestor_of_a_binary_tree.py
+++ leet_code/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -16,9 +16,27 @@
 # Another example is LCA of nodes 5 and 4 is 5,
 # since a node can be a descendant of itself according to the LCA definition.
 
-class LowestCommon(object):
-    def solution1(self):
 
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+class LowestCommon(object):
+    def solution1(self, root, vnode, wnode):
+        if not root or not vnode or not wnode:
+            return None
+        return self.recursive(root, vnode, wnode)
+
+    def recursive(self, root, vnode, wnode):
+        if not root or root.val == vnode.val or root.val == wnode.val:
+            return root
+        right = self.recursive(root.right, vnode, wnode)
+        left = self.recursive(root.left, vnode, wnode)
+        if right and left:
+            return root
+        return left if not right else right
 
 if __name__ == '__main__':
     pass
