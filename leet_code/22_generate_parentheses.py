6696a70249c7d7381095c22b336822f59dceea1b
Hohenheim <eph.oliver@gmail.com>
2017-11-12 13:23:24 +0800

    finish 22

--- leet_code/22_generate_parentheses.py
+++ leet_code/22_generate_parentheses.py
@@ -11,8 +11,37 @@
 # ]
 
 class GenerateParentheses(object):
-    def solution1(self):
-        pass
+    def solution1(self, num):
+        '''
+        
+        :param num: 
+        :return: 
+        '''
+        result = set()
+        if num < 1:
+            return list(result)
+        left, right = num, num
+        self.recursive(left, right, '', result)
+        result = list(result)
+        return result
+
+    def recursive(self, right, left, content, result):
+        if left > 0:
+            content += '('
+            left -= 1
+            self.recursive(right, left, content, result)
+            content = content[0:len(content) - 1]
+            left += 1
+        if right > 0 and right > left:
+            content += ')'
+            right -= 1
+            self.recursive(right, left, content, result)
+        if right == 0 and left == 0:
+            result.add(content)
+
+
+
 
 if __name__ == '__main__':
-        pass
+    result = GenerateParentheses().solution1(3)
+    print result
