50c86cc0dbc91e8c81cbcf147865042c77627bd4
Hohenheim <eph.oliver@gmail.com>
2017-12-27 23:28:22 +0800

    finish 396

--- leet_code/122_best_time_to_buy_and_sell_stock_II.py
+++ leet_code/122_best_time_to_buy_and_sell_stock_II.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Say you have an array for which the ith element is the price of a given stock on day i.
 #
 # Design an algorithm to find the maximum profit.
@@ -18,8 +19,10 @@ class BestTimeToBuyAndSellII(object):
                 min = item
         return sum
 
+
 if __name__ == '__main__':
     # nums = [7, 1, 5, 3, 6, 4]
     nums = [7, 6, 4, 3, 1]
     result = BestTimeToBuyAndSellII().solution1(nums)
     print result
+
